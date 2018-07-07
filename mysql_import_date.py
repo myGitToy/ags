72a8820cb108d1ecb4208642abfe757169eef75e
Hui Qiao <g.huiqiao@gmail.com>
2018-07-07 15:11:17 +0000

    修改一处拼写错误

--- mysql_import_date.py
+++ mysql_import_date.py
@@ -10,6 +10,7 @@ import crew_link_line
 #########导入航线信息：拆分机组  这是所有分析的基础和前提
 #数据导入
 crew_link_line.import_data()
+print("crew link line process complete")
 
 #########导入ags_event
 #月度事件匹配
@@ -17,15 +18,19 @@ ags_event.match()
 #数据导出
 ags_event.export_ags_event_summary(start_date='2018-6-1',end_date='2018-6-30')
 ags_event.export_ags_event_person(start_date='2018-6-1',end_date='2018-6-30')
+print("ags event process complete")
 
 #########导入ags_snapshot
 #数据清洗
 ags_snapshot.clean()
 #数据匹配
 ags_snapshot.match()
+print("ags snapshot process complete")
+
 
 #########导入flight_delay
 #将新增航班添加到延误数据库
 flight_delay.import_data
 #计算延误时间
 flight_delay.caculate_time()
+print("flight delay process complete")
