c87a674da6ff4a69a36e90879c3c529d9dee7d40
Hui Qiao <g.huiqiao@gmail.com>
2018-10-06 12:04:05 +0000

    添加静默模式的说明

--- ags_mail.py
+++ ags_mail.py
@@ -1,4 +1,31 @@
-# -*- coding:utf-8 -*-
+# -*- coding: utf-8 -*-
+"""
+文档说明【类】：
+    邮件处理模块，用于发送QAR品质报告
+    主要完成事件匹配和事件数据输出，为QAR分析提供csv格式的raw_data
+    引用规范：请使用下列语句
+        from  ags_mail import mail as mail #主模块引用
+        from profile.setup import setup_smtp as setup   #邮件配置文件引用
+    初始化：
+        ml=mail(name,month,email,start_date,end_date,text_msg) 注：实际代码中目前并没有使用start_date和end_date
+    发送邮件：
+        ml.send()
+
+基本框架：
+    __init__   初始化
+    send    发送邮件
+
+版本信息：
+    version 0.1
+    乔晖 2018/9/22
+
+修改日志：
+    #2018/9/5
+    1. 【邮件】增加ags_mail.py，添加邮件发送功能
+
+TODO LIST：
+    目前邮件发送是通过本模块调用ags_report.xxx来获取邮件正文的，未来可能计划采用引入参数的模式，需要修改__init__
+"""
 import smtplib
 import email
 from email.mime.multipart import MIMEMultipart
@@ -12,9 +39,9 @@ import ags_report
 #from ags_report import report_person_v1
 #import ags_report as rpt
 
-class mail:
-    def __init__(self,name,month,email,start_date,end_date):
-        '''
+class mail(object):
+    def __init__(self,name,month,email,start_date,end_date,text):
+        """
         [数据初始化]######
         函数说明 创建：乔晖 2018/9/5
         [输入Parameters]:
@@ -26,13 +53,14 @@ class mail:
         -------
         [返回值return]：
         无
-        '''
+        """
         #类创建时需要输入的参数
         self.c_name=name
         self.m_month=month
         self.rcptto=email
         self.start_date=start_date
         self.end_date=end_date
+        self.text=text
         
         #其他系统默认参数
         # 发件人地址，通过控制台创建的发件人地址
@@ -45,12 +73,12 @@ class mail:
         #注：目前邮件列表测试失败，只能单一邮件地址进行发送
         #rcptto = ['***', '***']
         #self.rcptto ='g.huiqiao@aliyun.com'
-    
+        print("初始化%s" % self.rcptto)
     def send_test(self):
         print(self.rcptto)
     
     def send(self):
-        '''
+        """
         [发送邮件(SMTP核心代码)]######
         函数说明 创建：乔晖 2018/9/12
         [输入Parameters]:
@@ -60,18 +88,18 @@ class mail:
         [返回值return]：
         True:邮件发送成功
         Flase:邮件发送失败
-        '''
+        """
         # 构建alternative结构
         msg = MIMEMultipart('alternative')
-        msg['Subject'] = Header('QAR个人月度分析材料')
+        msg['Subject'] = Header('【测试】QAR个人月度分析材料')
         msg['From'] = '%s <%s>' % (Header('QAR月报'), self.username)
         msg['To'] = self.rcptto
         msg['Reply-to'] = self.replyto
         msg['Message-id'] = email.utils.make_msgid()
         msg['Date'] = email.utils.formatdate() 
         # 构建alternative的text/plain部分
-        text_msg=ags_report.report_person_v1(self.c_name,self.m_month)
-        
+        #text_msg=ags_report.report_person_v1(self.c_name,self.m_month)
+        text_msg=self.text
         
         textplain = MIMEText(text_msg, _subtype='plain', _charset='UTF-8')
         msg.attach(textplain)
@@ -83,7 +111,7 @@ class mail:
         # 构造MIMEBase对象做为文件附件内容并附加到根容器
         contype = 'application/octet-stream'
         maintype, subtype = contype.split('/', 1)
-        '''
+        """
         ## 读入文件内容并格式化
         #with open('~/environment/TuShare/data/today_all.csv', mode='w', encoding='UTF-8', errors='strict', buffering=1) as file:
         #    file.write(result)
@@ -99,7 +127,7 @@ class mail:
         file_msg.add_header('Content-Disposition',
          'attachment', filename = basename)
         main_msg.attach(file_msg)
-        '''
+        """
         
         # 发送邮件
         try:
@@ -108,8 +136,8 @@ class mail:
             #client = smtplib.SMTP_SSL()
             #SMTP普通端口为25或80
             client.connect('smtpdm.aliyun.com', 25)
-            #开启DEBUG模式
-            client.set_debuglevel(0)
+            #开启DEBUG模式 0:静默 1:详细信息
+            client.set_debuglevel(1)
             client.login(self.username, self.password)
             #发件人和认证地址必须一致
             #备注：若想取到DATA命令返回值,可参考smtplib的sendmaili封装方法:
